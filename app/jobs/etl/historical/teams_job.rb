6e1fe6362cdc82dffcaa18ea42a4ee46bbd735cd
Eric Oligney <eoengineer@gmail.com>
2022-03-18 00:08:30 -0400

    fix indentation

--- app/jobs/etl/historical/teams_job.rb
+++ app/jobs/etl/historical/teams_job.rb
@@ -1,6 +1,6 @@
 class Etl::Historical::TeamsJob < ApplicationJob
   def perform()
-    current_year      = Date.today.year
+    current_year   = Date.today.year
     five_years_ago = 5.years.ago.year
     (five_years_ago...current_year).to_a.each do |year|
       Etl::Teams::AllTeamsJob.perform_later(year)

7e2b009aeedb8e19e073500a1fe278838d243498
Eric Oligney <eoengineer@gmail.com>
2022-04-05 15:55:01 -0400

    integrated bootstrap, added nav bar componenet

--- app/javascript/packs/app.jsx
+++ app/javascript/packs/app.jsx
@@ -1,22 +1,26 @@
 // Run this example by adding <%= javascript_pack_tag 'hello_react' %> to the head of your layout file,
 // like app/views/layouts/application.html.erb. All it does is render <div>Hello React</div> at the bottom
 // of the page.
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import React from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 
+import AppNavBar from '../src/components/layouts/app_navbar'
 import { TeamsContext, TeamsDataProvider } from '../src/contexts/teams_context'
 
 import TeamsIndex from '../src/views/teams/index'
 
 const App = () => (
-  <TeamsIndex />
+  // <TeamsIndex />
+  <div>MLB Data Home Page</div>
 )
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
     <TeamsDataProvider>
+      <AppNavBar />
       <App />
     </TeamsDataProvider>,
     document.body.appendChild(document.createElement('div')),
